a01c6988c722effb00b826f46e75e638f5e85387
ronnyonjetech <nyonjeron@gmail.com>
2025-03-12 09:58:48 +0300

    dockerized dashboard

--- afrijourdashboard3/vite.config.ts
+++ afrijourdashboard3/vite.config.ts
@@ -1,6 +1,7 @@
-// import path from 'path'
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
+
+// import path from 'path';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
@@ -10,7 +11,30 @@
 //       '@': path.resolve(__dirname, './src'),
 //     },
 //   },
-// })
+//   server: {
+//     proxy: {
+//       '/journal_api': {
+//         target: 'http://198.211.110.243',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/journal_api/, ''),
+//       },
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           vendor: ['react', 'react-dom'],
+//         },
+//       },
+//     },
+//     chunkSizeWarningLimit: 1000,
+//     sourcemap: true,
+//   },
+// });
+
+
+
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
@@ -24,13 +48,16 @@ export default defineConfig({
     },
   },
   server: {
-    proxy: {
-      '/journal_api': {
-        target: 'http://198.211.110.243',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/journal_api/, ''),
-      },
-    },
+    host: true, // Allow access from the local network (0.0.0.0)
+    port: 5173, // Force Vite to use port 5173
+    strictPort: true, // Prevent Vite from switching to another port if 5173 is in use
+    // proxy: {
+    //   '/journal_api': {
+    //     target: 'http://198.211.110.243',
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/journal_api/, ''),
+    //   },
+    // },
   },
   build: {
     rollupOptions: {
